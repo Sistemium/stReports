2e36dddc6915181f064ba7d817a7e29a2d410f41
Albert <kovalevskij.albert@gmail.com>
2016-05-12 15:59:52 +0300

    remove unused middleware

--- server/config/express.js
+++ server/config/express.js
@@ -10,12 +10,9 @@ import morgan from 'morgan';
 import compression from 'compression';
 import bodyParser from 'body-parser';
 import methodOverride from 'method-override';
-import cookieParser from 'cookie-parser';
 import errorHandler from 'errorhandler';
 import path from 'path';
-import lusca from 'lusca';
 import config from './environment';
-import session from 'express-session';
 
 export default function(app) {
   var env = app.get('env');
@@ -26,35 +23,6 @@ export default function(app) {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(methodOverride());
-  app.use(cookieParser());
-
-  // Persist sessions with mongoStore / sequelizeStore
-  // We need to enable sessions for passport-twitter because it's an
-  // oauth 1.0 strategy, and Lusca depends on sessions
-  app.use(session({
-    secret: config.secrets.session,
-    saveUninitialized: true,
-    resave: false
-  }));
-
-  /**
-   * Lusca - express server security
-   * https://github.com/krakenjs/lusca
-   */
-  if ('test' !== env) {
-    app.use(lusca({
-      csrf: {
-        angular: true
-      },
-      xframe: 'SAMEORIGIN',
-      hsts: {
-        maxAge: 31536000, //1 year, in seconds
-        includeSubDomains: true,
-        preload: true
-      },
-      xssProtection: true
-    }));
-  }
 
   app.set('appPath', path.join(config.root, 'client'));
 
@@ -62,10 +30,6 @@ export default function(app) {
     app.use(favicon(path.join(config.root, 'client', 'favicon.ico')));
     app.use(express.static(app.get('appPath')));
     app.use(morgan('dev'));
-  }
-
-  if ('development' === env) {
-    app.use(require('connect-livereload')());
   }
 
   if ('development' === env || 'test' === env) {
