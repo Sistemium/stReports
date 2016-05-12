ba27b27b304a96b6f2650a3247a86165c1502039
Albert <kovalevskij.albert@gmail.com>
2016-05-12 17:21:45 +0300

    get logs

--- server/api/logs/log.controller.js
+++ server/api/logs/log.controller.js
@@ -1,5 +1,15 @@
 'use strict';
 
+var stapi = require('../../STAPI/model');
+var log = stapi('prt/log');
+
 export function index(req, res) {
- return res.json([]);
+  log(req).find()
+    .then((response) => {
+      return res.json(response);
+    })
+    .catch((err) => {
+      return res.statusCode(500);
+    })
+  ;
 }
