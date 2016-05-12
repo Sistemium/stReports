2b37162f6e218edb95449878f35c74c59b30fe1c
Albert <kovalevskij.albert@gmail.com>
2016-05-12 11:03:25 +0300

    sql for Log table

---
+++ server/sql/prt-init.sql
@@ -0,0 +1,14 @@
+grant connect to prt;
+grant dba to prt;
+
+meta.defineType 'prt.url';
+meta.defineType 'prt.status';
+meta.defineType 'prt.filename';
+meta.defineType 'prt.fileSize:INTEGER';
+meta.defineType 'prt.processingTime:INTEGER';
+
+meta.defineEntity 'prt.Log',
+ 'url;status;filename;fileSize;processingTime'
+;
+
+meta.createTable 'prt.Log',0,1;
