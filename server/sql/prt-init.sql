45b42ccbd9fec5f9edeb9fb03a8853b8c4aeb836
Albert <kovalevskij.albert@gmail.com>
2016-05-17 17:13:24 +0300

    fileUrl field

--- server/sql/prt-init.sql
+++ server/sql/prt-init.sql
@@ -6,11 +6,12 @@ util.setUserOption 'asamium.default.domain', 'prt';
 meta.defineType 'url';
 meta.defineType 'isConnectionAborted:BOOL';
 meta.defineType 'filename';
+meta.defineType 'fileUrl';
 meta.defineType 'fileSize:INTEGER,,nullable';
 meta.defineType 'processingTime:INTEGER,,nullable';
 
 meta.defineEntity 'Log',
- 'url;isConnectionAborted;filename;fileSize;processingTime'
+ 'url;isConnectionAborted;fileUrl;filename;fileSize;processingTime'
 ;
 
 meta.createTable 'Log',0,1;
