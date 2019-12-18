80a442fd8fa0d239ae8ea301aa6f3b8da7ec21ce
Alexander Levin <alevin.ru@gmail.com>
2019-12-18 22:01:55 +0200

    rm gulp

--- server/index.js
+++ server/index.js
@@ -1,13 +1,5 @@
-'use strict';
+const env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-// Set default node environment to development
-var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+console.log('starting', env);
 
-if (env === 'development' || env === 'test') {
-  // Register the Babel require hook
-  require('babel-core/register');
-}
-
-// Export the application
-//noinspection JSUnresolvedVariable
-exports = module.exports = require('./app');
+import './app';
